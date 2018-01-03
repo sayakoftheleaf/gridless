42fd0983ae34cde079a9d28125bdc0f0b418a450
sayakoftheleaf <sayakchatterjee1309@gmail.com>
2018-01-02 19:14:56 -0500

    got heavy animation to work

--- Heavy.gd
+++ Heavy.gd
@@ -2,7 +2,7 @@ extends KinematicBody2D
 
 #constants
 const BASE_HEALTH = 600
-const DEFAULT_SPEED = 4000
+const DEFAULT_SPEED = 3000
 
 #variables
 var speedx
@@ -11,37 +11,44 @@ var directionx
 var directiony
 var velocity = Vector2()
 var sprite
+var walking_animations
 
 func _ready():
 	set_process(true)
 	sprite = get_node("Sprite")
+	walking_animations = get_node("Sprite/AnimationPlayer")
+	walking_animations.set_animation_process_mode(0)
 	pass
+	
+func return_pos():
+	return get_pos()
 	
 func _process(delta):
 	
-	directionx = -1
-	directiony = 0
+	directionx = 0
+	directiony = -1
 	
 	# setting head direction
-	if (directionx == 1):
-		sprite.set_frame(2)
-	elif (directionx == -1):
-		sprite.set_frame(2)
-	elif (directiony == -1):
-		sprite.set_frame(0)
-	elif (directiony == 1):
-		sprite.set_frame(1)
+	if (directionx == 1): 
+		print ("here")
+		walking_animations.play("topmove")
+	elif (directionx == -1): walking_animations.play("topmove")
+	elif (directiony == -1 && walking_animations.get_current_animation()!= "topmove"): 
+		print ("ty god")
+		walking_animations.play("topmove")
+		print(walking_animations.is_playing())
+	elif (directiony == 1): walking_animations.play("topmove (copy)")
 		
 	velocity = Vector2(DEFAULT_SPEED * directionx, DEFAULT_SPEED * directiony)
 	velocity = velocity * delta * delta
 	move(velocity)
 	pass
 	
-func ground_pound(playa):
-	var attack = get_node("shoc/Area2D")
-	var dmg = 0
-	if (attack.overlaps_body(playa)):
-		dmg = attack.return_dmg()
+#func ground_pound(playa):
+#	var attack = get_node("shoc/Area2D")
+#	var dmg = 0
+#	if (attack.overlaps_body(playa)):
+#		dmg = attack.return_dmg()
 		
-	return dmg
+#	return dmg
 	
--- Main_Engine.gd
+++ Main_Engine.gd
@@ -1,17 +1,15 @@
 extends Node2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
 var player
 var heavies = []
 var melees = []
 var total_dmg
-
+var path
+var navnode
 
 func _ready():
 	player = get_node("GR3N")
+	navnode = get_node("Navigation2D")
 	#temp
 	heavies.append(get_node("Heavy"))
 	set_process(true)
@@ -22,9 +20,10 @@ func _process(delta):
 	total_dmg = 0
 	
 	for x in heavies:
-		total_dmg = x.ground_pound(player)
+		#total_dmg = x.ground_pound(player)
+		path = navnode.get_simple_path(x.return_pos(), player.get_pos())
+		print(path)
 	
 	player.modify_health(total_dmg)
-	print (player.current_health())
 	pass
 	
